9492fae2469df24f60c190d38a62fc8c763bd799
roracion <roracionbusiness1@gmail.com>
2024-08-22 21:21:11 +0930

    Update server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
-const sequelize = require('./models');
+const { Pool } = require('pg')
 
-sequelize.sync({ force: false })
-  .then(() => {
-    console.log('Database & tables created!');
-  });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}).get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
